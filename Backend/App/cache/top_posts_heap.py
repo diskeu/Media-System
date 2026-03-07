75a33b78f7cd752d730df73d5b0a208e8cb21150
Tim Jelenz <jelenzt@gmail.com>
2026-03-07 11:58:15 +0100

    Fix bugs in heapify_down and heapify_up func and add post_lookup

--- Backend/App/cache/top_posts_heap.py
+++ Backend/App/cache/top_posts_heap.py
@@ -16,9 +16,9 @@ class TopNHeap():
         arr must be a list of tuples with [0] = post_id and [1] = hotness
         """
         self.arr = arr
-        self.sorted_cache = None
         self.max_size = max_size
         self.update_intervall = update_intervall
+        self.post_lookup = {} # {post_id: heap_index}
         
     async def update_heap_tracker(self):
         while True:
@@ -28,18 +28,24 @@ class TopNHeap():
         smallest = cur_i
         len_arr = len(self.arr)
         while True:
-            prev_smallest = smallest
+            smallest = cur_i
             # calculating left and right index
             left = 2 * cur_i + 1
             right = 2 * cur_i + 2
 
             # setting smallest if left or right exists
-            if left < len_arr and self.arr[left][1] < self.arr[smallest][1]: smallest = self.arr[left][1]
-            if right < len_arr and self.arr[right][1] < self.arr[smallest][1]: smallest = self.arr[right][1]
+            if left < len_arr and self.arr[left][1] < self.arr[smallest][1]: smallest = left
+            if right < len_arr and self.arr[right][1] < self.arr[smallest][1]: smallest = right
 
             # swapping values if a new smallest occured
-            if smallest == prev_smallest: return
-            self.arr[prev_smallest], self.arr[smallest] = self.arr[smallest], self.arr[prev_smallest]
+            if smallest == cur_i: return
+            self.arr[cur_i], self.arr[smallest] = self.arr[smallest], self.arr[cur_i]
+
+            # updating values in post_lookup
+            self.post_lookup[self.arr[cur_i][0]] = cur_i
+            self.post_lookup[self.arr[smallest][0]] = smallest
+
+            cur_i = smallest
 
     def build_max_heap(self):
         # getting index of last element with a child
@@ -55,19 +61,30 @@ class TopNHeap():
     def heapify_up(self, cur_i):
         while True:
             parent_i = (cur_i - 1) // 2
-            if self.arr[parent_i][1] <= self.arr[cur_i][1] or parent_i < 0: return
+            if parent_i < 0 or self.arr[parent_i][1] <= self.arr[cur_i][1]: return cur_i # no swapping
+
             # swapping parent with child
             self.arr[cur_i], self.arr[parent_i] = self.arr[parent_i], self.arr[cur_i]
+
+            # updating values in post_lookup
+            self.post_lookup[self.arr[cur_i][0]] = cur_i
+            self.post_lookup[self.arr[parent_i][0]] = parent_i
+            
             cur_i = parent_i
 
     def insert(self, item: tuple[int, int]):
         """Function to insert tuple[post_id, hotness]"""
-        self.sorted_cache = None # removing stored arr
 
         # checking if the array is full
         if self.max_size <= len(self.arr):
             if item[1] <= self.arr[0][1]: return # returning if hotness is too small
+
+            # removing old item
+            self.post_lookup.pop(self.arr[0][0])
+
             self.arr[0] = item                   # removing unhottest item
+            self.post_lookup[item[0]] = 0        # item is now on top of the arr
+
             self.heapify_down(0)
         else:
             self.arr.append(item)

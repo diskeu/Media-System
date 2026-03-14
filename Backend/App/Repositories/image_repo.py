12652aa8d90dfdae15b1fc83b1025f5d699a7ad5
Tim Jelenz <jelenzt@gmail.com>
2026-03-14 20:32:01 +0100

    refactor: make image_repo asynchronous
    - updates all functions to be asynchronous to await other asynchronous functions

--- Backend/App/Repositories/image_repo.py
+++ Backend/App/Repositories/image_repo.py
@@ -9,12 +9,12 @@ class ImageRepo(BaseRepo):
         self.logger = logger
         self.cnx = cnx
     
-    def add_images_to_post(self, *models: Vote) -> None | BaseRepo.RepoError:
-        return self.post_model(     # None | RepoError
+    async def add_images_to_post(self, *models: Vote) -> None | BaseRepo.RepoError:
+        return await self.post_model(     # None | RepoError
             "messenger.images",
             *models
         )
-    def get_images_path(self, image_ids: list[int] | None, post_ids: list[int] | None) -> list[dict[any]] | BaseRepo.RepoError:
+    async def get_images_path(self, image_ids: list[int] | None, post_ids: list[int] | None) -> list[dict[any]] | BaseRepo.RepoError:
         """
         Gets all paths of the images of the specific image_ids\n
         Pare image_ids or post_ids not both
@@ -26,12 +26,12 @@ class ImageRepo(BaseRepo):
             TypeError("Too many arguments got parsed")
         )
         self.logger.exception("Too many arguments got parsed")
-        return self.get_all_enriched(
+        return await self.get_all_enriched(
             table="messenger.images",
             primary_keys=("image_id", [(image_id, ) for image_id in image_ids]) if image_ids else ("post_id", [(post_id, ) for post_id in post_ids]),
             columns=["image_path"]
         )
-    def delete_image(self, *image_ids: int) -> None | BaseRepo.RepoError:
+    async def delete_image(self, *image_ids: int) -> None | BaseRepo.RepoError:
         """Given a list of image_ids, deletes the corresponding images"""
         # making condition
         statement = ["%s" for _ in range(len(image_ids))]
@@ -43,4 +43,4 @@ class ImageRepo(BaseRepo):
             condition=condition
         )
         # executing statement
-        return self.execute_write(delete_query, *image_ids)
+        return await self.execute_write(delete_query, *image_ids)

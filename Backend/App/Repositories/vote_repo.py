5bd769d264b9a4821905570f3a6182cb1e6d785f
Tim Jelenz <jelenzt@gmail.com>
2026-03-14 20:41:45 +0100

    refactor: make: vote_repo asynchronous & fixing return
    - update all funcs to be asynchronous and to await other asynchronous funcs
    - fixing return to always return a RepoError if one occures

--- Backend/App/Repositories/vote_repo.py
+++ Backend/App/Repositories/vote_repo.py
@@ -9,12 +9,12 @@ class VoteRepo(BaseRepo):
         self.logger = logger
         self.cnx = cnx
     
-    def get_users_vote(self, user_id, *post_ids: str) -> dict[int, int] | BaseRepo.RepoError:
+    async def get_users_vote(self, user_id, *post_ids: str) -> dict[int, int] | BaseRepo.RepoError:
         """
         Given a user and posts, returns the status of the users vote in format dict \n
         -> {post_id: -1 | 1} for all values where user has placed a vote or RepoError
         """
-        sql_return_val: list = self.get_all_enriched(
+        sql_return_val: list = await self.get_all_enriched(
             table="messenger.votes",
             where_statement="user_id = %s",
             primary_keys=("post_id", [post_ids]),
@@ -27,7 +27,7 @@ class VoteRepo(BaseRepo):
         post_id_to_vote = {post_vote.get("post_id"): post_vote.get("vote") for post_vote in sql_return_val}
         return post_id_to_vote
 
-    def vote(self, user_id, post_id, mode: int) -> None | BaseRepo.RepoError:
+    async def vote(self, user_id, post_id, mode: int) -> None | BaseRepo.RepoError:
         """
         Given an user_id, post_id and mode will do a vote related task depending on the vote\n
         Modes:
@@ -40,7 +40,7 @@ class VoteRepo(BaseRepo):
                 "messenger.votes",
                 "WHERE user_id = %s AND post_id = %s"
             )
-            return self.execute_write(delete_query, user_id, post_id) # None | RepoError
+            return await self.execute_write(delete_query, user_id, post_id) # None | RepoError
 
         vote_instance = Vote(
             user_id,
@@ -58,4 +58,4 @@ class VoteRepo(BaseRepo):
         insert_vals.append(vote_instance.vote)
         insert_query += " ON DUPLICATE KEY UPDATE vote = %s;"
 
-        self.execute_write(insert_query, *insert_vals)
+        return await self.execute_write(insert_query, *insert_vals) # None | RepoError

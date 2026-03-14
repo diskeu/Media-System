b0a136c7c518645815eb97bf2581ec4de3b8de31
Tim Jelenz <jelenzt@gmail.com>
2026-03-14 17:04:08 +0100

    add: add_images_to_post, get_images_path and delete_image functions

--- Backend/App/Repositories/base_repo.py
+++ Backend/App/Repositories/base_repo.py
@@ -157,31 +157,31 @@ class BaseRepo():
         where_statement: str = None,
         condition: str = None,
         values: Iterable | None = None,
-        columns=None
+        columns: Iterable = None
     ) -> list[dict[any]] | RepoError:
         """
         Given a table and primary_keys returns all necessary information from the specified columns\n
         Primary Keys have to be in example format -> (("post_id", "community_id"), [("1", "10"), ("2", "15"), ("3", "20")]) or None\n
-        Note: In contrast to the normal get_info function it is intended to have acces to other columns via. JOIN.\n
-        It is built for more than one post and the return isn't intendet to be formatted into Models\n
-        NOTE: If primary_keys[0] is only 1 value, parse an non-iterable value
-
-        NOTE: values 'll be added in order auto-generated primary key values -> specified values
+        NOTE:
+            - In contrast to the normal get_info function it is intended to have acces to other columns via. JOIN.\n
+            - It is built for more than one post and the return isn't intendet to be formatted into Models\n
+            - If primary_keys[0] is only 1 value, parse an non-iterable value\n
+            - values 'll be added in order auto-generated primary key values -> specified values\n
 
         ## joins ##
-        #### To use columns from other Tables you have to type a join statement into the joins parameter####
-        The joins have to be formatted like this
+        #### To use columns from other Tables you have to type a join statement into the joins parameter ####
+        The joins have to be formatted like this\n
         joins = [
             ("INNER JOIN votes v", "p.post_id = v.post_id"),
             ("LEFT JOIN images i", "p.post_id = i.post_id")
-        ]
+        ]\n
         NOTE: Joins should'nt contain %s placeholders
         ## where_statement ##
         additional where statement that 'll get added
         
         ## condition ##
-        Additonal conditon that 'll be added after the primary - key statement
-        Condition should be formatted with %s -> 'll be later replaced with the values (the formatt vals of %s need to be given into values parm)\n
+        Additonal conditon that 'll be added after the primary - key statement\n
+        Condition should be formatted with %s -> 'll be later replaced with the values (the formatt vals of %s need to be given into values parm)
 
         ## Returns: ###
         Returns the sql connector return, in dict format | RepoError.
---
+++ Backend/App/Repositories/image_repo.py
@@ -0,0 +1,46 @@
+# Repo to access / post data, related to the images-table
+from __future__ import annotations
+from Backend.App.Repositories.base_repo import BaseRepo
+from Backend.App.Models.vote import Vote
+
+class ImageRepo(BaseRepo):
+    def __init__(self, logger, cnx):
+        super().__init__(logger)
+        self.logger = logger
+        self.cnx = cnx
+    
+    def add_images_to_post(self, *models: Vote) -> None | BaseRepo.RepoError:
+        return self.post_model(     # None | RepoError
+            "messenger.images",
+            *models
+        )
+    
+    def get_images_path(self, image_ids: list[int] | None, post_ids: list[int] | None) -> list[dict[any]] | BaseRepo.RepoError:
+        """
+        Gets all paths of the images of the specific image_ids\n
+        Pare image_ids or post_ids not both
+        """
+        if image_ids and post_ids: return self.RepoError(
+            False,
+            9,
+            "You can only parse image id or post id",
+            TypeError("Too many arguments got parsed")
+        )
+        return self.get_all_enriched(
+            table="messneger.images",
+            primary_keys=("image_id", [(image_id, ) for image_id in image_ids]) if image_ids else ("post_id", [(post_id, ) for post_id in post_ids]),
+            columns=["image_path"]
+        )
+    def delete_image(self, *image_ids: int) -> None | BaseRepo.RepoError:
+        """Given a list of image_ids, deletes the corresponding images"""
+        # making condition
+        statement = ["%s" for _ in range(len(image_ids))]
+        condition = f"WHERE image_id IN ({", ".join(statement)})"
+
+        # getting delete query
+        delete_query = self.build_delete_query(
+            table="messenger.images",
+            condition=condition
+        )
+        # executing statement
+        return self.execute_write(delete_query, *image_ids)

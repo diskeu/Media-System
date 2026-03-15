33fe66d0f6f8f4df2135813c2bc6eced1eaecb0e
Tim Jelenz <jelenzt@gmail.com>
2026-03-15 18:24:08 +0100

    refactor: update 'image_repo_test' case to be asynchronous
    commit 'll
        - update test case for image_repo to be asynchronous

--- tests/repositories/image_repo_test.py
+++ tests/repositories/image_repo_test.py
@@ -3,10 +3,20 @@ from Backend.App.Models.image import Image
 from Backend.App.logger_config import setup_logger
 from Backend.App.Database.connection import connect
 from utils.sentinel import DEFAULT
+import asyncio
 
-c_r = ImageRepo(setup_logger(), connect("/Users/TimJelenz/Desktop/messenger/Backend/Configurations/mysql.conf", "root"))
-v1 = Image(DEFAULT, "Users/TimJelenz/xy", 44)
-v2 = Image(DEFAULT, "Users/TimJelenz/xy", 44) 
-print(c_r.get_images_path(image_ids=None, post_ids=[44]))
-print(c_r.get_images_path(image_ids=[1, 3], post_ids=None))
-print(c_r.get_images_path(image_ids=[1, 2], post_ids=[3, 4]))
+async def connect_test():
+    connection = await connect(
+        "/Users/TimJelenz/Desktop/messenger/Backend/Configurations/mysql.conf", "root"
+    )
+    c_r = ImageRepo(
+        setup_logger(),
+        connection
+    )
+    v1 = Image(DEFAULT, "Users/TimJelenz/xy", 44)
+    v2 = Image(DEFAULT, "Users/TimJelenz/xy", 44) 
+    print(c_r.get_images_path(image_ids=None, post_ids=[44]))
+    print(c_r.get_images_path(image_ids=[1, 3], post_ids=None))
+    print(c_r.get_images_path(image_ids=[1, 2], post_ids=[3, 4]))
+
+asyncio.run(connect_test())

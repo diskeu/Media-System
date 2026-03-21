30d730dd6b06a6bc6cd9499d755ded45541bf975
Tim Jelenz <jelenzt@gmail.com>
2026-03-21 22:04:29 +0100

    refactor: Mail Sender's initalisation process into func
    - puts the authorization process into external function
        * authorization is needed by every new send because of the expiring tokens
    - the initalisation doesn't do authorisation anymore because it doesn't need the creds
    - send_mail function still in progress

--- Backend/App/Services/Auth_Service/google_mail_sender.py
+++ Backend/App/Services/Auth_Service/google_mail_sender.py
@@ -22,9 +22,6 @@ class MailSender():
             credentials_f_location="/Users/TimJelenz/Desktop/messenger/Backend/Configurations/credentials.json",
             ):
         """
-        Loads the acces tokens and trys to connect to the Apps-Script-Api.
-        'Backend/Configurations/token.json' stores the User's access and refresh tokens,
-        and is created automatically when the authorization flow completes for the first time.\n
         NOTE: Class should only used once in initalisation and parsed to other funcs because it
         isn't asynchron.
         
@@ -32,28 +29,38 @@ class MailSender():
             - port -> port where flow 'll get run
             - token_f_location -> location where the acces/refresh tokens will be/are stored
             - credentials_f_location -> location where the app identity get's stored
-
-        Raises:
-            - google.auth.exceptions.RefreshError if the scope is invalid
         """
         self.port = port
         self.token_f_location = token_f_location
         self.credentials_f_location = credentials_f_location
 
+    def authenticate(self) -> Credentials:
+        """
+        Loads the acces tokens and trys to connect to the Apps-Script-Api.
+        When the creds in self.token_f_location are valid the authorization finishes without doing further steps.
+        'Backend/Configurations/token.json' stores the User's access and refresh tokens,
+        and is created automatically when the authorization flow completes for the first time.
+
+        Raises:
+            - google.auth.exceptions.RefreshError if the scope is invalid
+
+        Returns:
+            - the Credentials to operate with the api
+        """
         creds = None
-        if os.path.exists("Backend/Configurations/token.json"):
-            creds = Credentials.from_authorized_user_file(token_f_location, MailSender.SCOPES)
+        if os.path.exists(self.token_f_location):
+            creds = Credentials.from_authorized_user_file(self.token_f_location, MailSender.SCOPES)
 
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                credentials_f_location, MailSender.SCOPES
+                self.credentials_f_location, MailSender.SCOPES
                 )
-                creds = flow.run_local_server(port=port)
+                creds = flow.run_local_server(port=self.port)
             # writing changes to file
-            with open(token_f_location, "w") as token_f:
+            with open(self.token_f_location, "w") as token_f:
                 token_f.write(creds.to_json())
 
     def update_scope(self,*args) -> None:
@@ -70,7 +77,16 @@ class MailSender():
         with open(self.token_f_location, "w") as token_f:
             token_f.write(creds.to_json())
 
-    def send_mail(user_name: str):
+    def send_mail(user_name: str, user_email: str):
         """Sends Mail using the defined mail in verification_mail.py"""
         body = build_verification_mail(user_name)
+        msg = MIMEMultipart()
+        msg["Subject"] = "Confirm your Media-System account"
+        msg["FROM"] = "marvinmagmud@gmail.com"
+        msg["TO"] = user_email
+
+        # add HTML
+        msg.attach(MIMEText(body, "html"))
+
+        # 
         

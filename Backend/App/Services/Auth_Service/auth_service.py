b2c26530edd5263008370e88c33178a8feb4251c
Tim Jelenz <jelenzt@gmail.com>
2026-03-23 20:23:44 +0100

    refactor: remove email-verification TODO
    - remove of the email-verification TODO because it has already been implemented

--- Backend/App/Services/Auth_Service/auth_service.py
+++ Backend/App/Services/Auth_Service/auth_service.py
@@ -96,23 +96,6 @@ class AuthService():
             thread_pool=self.thread_pool
         )
 
-        # TODO -> Sending register email
-        #   - find framework to send mails
-        #   - need to keep track of activation codes
-        #   - Need to keep track which Users need email-conformation
-        #       * either register them right away into the DB and limit their actions
-        #           * register them into the DB and having a background job everyday popping the users that took to lon
-        #           * havinsg a async func that keeps track of all users in pending state
-        #               * if the timer reaches it's end the corresponding user 'll get popped
-        #       * having a table for the pending users would eliminate checking if the user is pending
-        #       * or the user doesn't exist in the DB until the email get's confirmed
-
-        # Table with pending users
-        # Background Job runs on the Table popping the users that have a long member since
-        # each time a user sucessfully confirms their email or reach the pending limit they 'll get popped
-        # when someone sends the code back it only works if the user is in the pending table
-        # each registration you need also to look into the pending table to look for unique vals | a correlation between the tables
-
     async def validate_token(self, token: str) -> bool: # TODO: Add rem / nrem token after sucessfull acc creation
         """
         Wrapper for verification_tokens.validate_token.

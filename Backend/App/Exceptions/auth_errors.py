93ede899ea2cdbecc87c028824991e05c81ccbae
Tim Jelenz <jelenzt@gmail.com>
2026-03-23 20:36:31 +0100

    refactor: make more compact versions of the error classes

--- Backend/App/Exceptions/auth_errors.py
+++ Backend/App/Exceptions/auth_errors.py
@@ -1,19 +1,15 @@
 class EmailAlreadyExistsError(BaseException):
-    def __init__(self, message = None):
-        if not message: message = "Email Already Exists"
+    def __init__(self, message = "Email Already Exists"):
         super().__init__(message)
 
 class UserNameAlreadyExistsError(BaseException):
-    def __init__(self, message = None):
-        if not message: message = "User Already Exists"
+    def __init__(self, message = "User Already Exists"):
         super().__init__(message)
 
 class InvalidPasswordError(BaseException):
-    def __init__(self, message = None):
-        if not message: message = "Password should Contain 8 - 20 char, atleast one upper & lower case letter, one number and one special char. Allowed char: A-Za-z, @$#%*!?"
+    def __init__(self, message = "Password should Contain 8 - 20 char, atleast one upper & lower case letter, one number and one special char. Allowed char: A-Za-z, @$#%*!?"):
         super().__init__(message)
 
 class InvalidEmailError(BaseException):
-    def __init__(self, message = None):
-        if not message: message = "Email is invalid"
+    def __init__(self, message = "Email is invalid"):
         super().__init__(message)
--- Backend/App/Exceptions/service_errors.py
+++ Backend/App/Exceptions/service_errors.py
@@ -1,5 +1,4 @@
 class NotNullError():
     """Raised when Null gets parsed into a Not Null field"""
-    def __init__(self, message = None):
-        if message == None: message = "Field cannot be Null"
+    def __init__(self, message = "Field cannot be Null"):
         super().__init__(message)
